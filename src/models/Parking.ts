3239800d5a1aa5364d96fec5bcf4de42b889b933
paulren-carpio <112433375+paulren-carpio@users.noreply.github.com>
2022-08-30 23:14:26 +0800

    Update Parking.ts

--- src/models/Parking.ts
+++ src/models/Parking.ts
@@ -23,16 +23,3 @@ export class Parking {
   @Column("timestamp", { nullable: true, name: "update_date" })
   updateDate: string;
 }
-
-// create database parking;
-
-// create table parking.parking_space (
-// 	`id` int(11) NOT NULL AUTO_INCREMENT,
-//     `slot_number` int(11) NOT NULL,
-//     `max_allowed_size` char(2) NOT NULL,
-//     `is_available` tinyint(1) NOT NULL DEFAULT 1,
-//     `vehicle_size` char(1),
-//     `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-//     `update_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-//     PRIMARY KEY (`id`)
-// );
